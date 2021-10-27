eb4667260729085f22c382f81f8eeb163cc86cae
Alpay Kücük <Alpaykucuk@outlook.de>
2021-10-27 21:53:16 +0200

    Initial Commit

---
+++ Polligo/PolligoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolligoApp.swift
+//  Polligo
+//
+//  Created by Alpay Kücük on 27.10.21.
+//
+
+import SwiftUI
+
+@main
+struct PolligoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
