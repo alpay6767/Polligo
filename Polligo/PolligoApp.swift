d4b11f52467a5ecd4c5db86c062418db5714fb84
Alpay Kücük <Alpaykucuk@outlook.de>
2021-10-27 23:13:57 +0200

    Firebase Anbindung fertig

--- Polligo/PolligoApp.swift
+++ Polligo/PolligoApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct PolligoApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
