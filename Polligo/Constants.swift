64054bb42ff5d4b790de082f115b447a2a3bd225
Alpay Kücük <Alpaykucuk@outlook.de>
2021-10-28 02:13:48 +0200

    Home fertig

---
+++ Polligo/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  Polligo
+//
+//  Created by Alpay Kücük on 28.10.21.
+//
+
+import Foundation
+
+class Constants {
+    
+    //MARK: -Strings für die App
+    static let logoString = "Polligo"
+    static let fontString = "OMEGLE"
+    static let colorString = "maincolor"
+    
+    static let mainQuestionString = "Was möchtest du heute tun?"
+    
+    //MARK: -Fragen für MainView
+    static let umfrageErstellenString = "Umfrage erstellen"
+    static let umfrageColorString = "umfragecolor"
+    
+    static let umfrageTeilnehmenString = "Umfrage teilnehmen"
+    static let teilnehmenColorString = "teilnehmencolor"
+}
--- Polligo/ContentView.swift
+++ Polligo/ContentView.swift
@@ -8,15 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var logo: some View {
+        
+        Text(Constants.logoString)
+            .font(Font.custom(Constants.fontString, size: 60, relativeTo: .title))
+            .padding(.top, 50)
+            .foregroundColor(.white)
+    }
+    
+    var header: some View {
+        Text(Constants.mainQuestionString)
+            .font(Font.custom(Constants.fontString, size: 22))
+            .foregroundColor(.white)
+    }
+    
+    var controls: some View {
+        Group {
+            HomeButton(name: Constants.umfrageErstellenString, buttonColor: Constants.umfrageColorString, textColor: .white) {
+                print("hi")
+            }
+            Spacer().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            HomeButton(name: Constants.umfrageTeilnehmenString, buttonColor: Constants.teilnehmenColorString, textColor: .white) {
+                print("hit")
+            }
+        }
+        
+    }
+    
     var body: some View {
         
-        Text("Polligo")
-            .font(Font.custom("BabySchoolItalic", size: 40))
+        VStack (spacing: 0) {
+        ZStack (alignment: .top) {
+                Color.init(Constants.colorString).ignoresSafeArea()
+                VStack {
+                    logo
+                    Spacer().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    header
+                    Spacer().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    controls
+                }
+            }
+        ZStack (alignment: .bottom){
+                Color.init(Constants.colorString).ignoresSafeArea()
+            Image("homeMen").resizable().aspectRatio(contentMode: .fit).padding(.bottom).frame(width: 300, height: 400, alignment: .bottom)
+        }.ignoresSafeArea()
+
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        //ContentView().preferredColorScheme(.dark)
+        ContentView().preferredColorScheme(.light)
+
     }
 }
